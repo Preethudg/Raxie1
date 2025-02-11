6e0d7cf64f875ab30380041bba7ab79cc6511bda
Preethudg <sanjanasanjanadg59@gmail.com>
2025-02-11 13:29:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
+
+import sys
+import glob
+import importlib
+from pathlib import Path
+from pyrogram import idle
 import logging
 import logging.config
-# Credit @LazyDeveloper.
-# Please Don't remove credit.
-# Born to make history @LazyDeveloper !
-# Thank you LazyDeveloper for helping us in this Journey
-# 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
-# for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
-# rip paid developers 🤣 - >> No need to buy paid source code while @LazyDeveloperr is here 😍😍
+import time  
+
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
@@ -18,64 +19,91 @@ logging.basicConfig(
 )
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-import os
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
+from database.ia_filterdb import Media, Media2, tempDict, choose_mediaDB, db as clientDB
 from database.users_chats_db import db
 from info import *
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from Script import script 
+from datetime import date, datetime 
+import pytz
 from aiohttp import web
-from plugins import web_server
+from plugins import web_server, check_expired_premium
 
 import asyncio
 from pyrogram import idle
-from lazybot import LazyPrincessBot
-
+from Deendayal_botz import DeendayalBot
 from util.keepalive import ping_server
-from lazybot.clients import initialize_clients
+from Deendayal_botz.clients import initialize_clients
+botStartTime = time.time()
 
-
-PORT = "8080"
-LazyPrincessBot.start()
+ppath = "plugins/*.py"
+files = glob.glob(ppath)
+DeendayalBot.start()
 loop = asyncio.get_event_loop()
 
-
-async def Lazy_start():
+async def Deendayal_start():
     print('\n')
-    print(' Initalizing Telegram Bot ')
-    if not os.path.isdir(DOWNLOAD_LOCATION):
-        os.makedirs(DOWNLOAD_LOCATION)
-    bot_info = await LazyPrincessBot.get_me()
-    LazyPrincessBot.username = bot_info.username
+    print('Initalizing Deendayal Dhakad Bot')
+    bot_info = await DeendayalBot.get_me()
+    DeendayalBot.username = bot_info.username
     await initialize_clients()
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem.replace(".py", "")
+            plugins_dir = Path(f"plugins/{plugin_name}.py")
+            import_path = "plugins.{}".format(plugin_name)
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+            load = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(load)
+            sys.modules["plugins." + plugin_name] = load
+            print("Deendayal dhakad Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server())
-    b_users, b_chats , lz_verified = await db.get_banned()
+    b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
-    temp.LAZY_VERIFIED_CHATS = lz_verified
     await Media.ensure_indexes()
-    me = await LazyPrincessBot.get_me()
+    await Media2.ensure_indexes()
+    stats = await clientDB.command('dbStats')
+    free_dbSize = round(512-((stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))), 2)
+    if DATABASE_URI2 and free_dbSize<62: #if the primary db have less than 62MB left, use second DB.
+        tempDict["indexDB"] = DATABASE_URI2
+        logging.info(f"Since Primary DB have only {free_dbSize} MB left, Secondary DB will be used to store datas.")
+    elif DATABASE_URI2 is None:
+        logging.error("Missing second DB URI !\n\nAdd SECONDDB_URI now !\n\nExiting...")
+        exit()
+    else:
+        logging.info(f"Since primary DB have enough space ({free_dbSize}MB) left, It will be used for storing datas.")
+    await choose_mediaDB()   
+    me = await DeendayalBot.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
-    LazyPrincessBot.username = '@' + me.username
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0" if ON_HEROKU else BIND_ADRESS
-    await web.TCPSite(app, bind_address, PORT).start()
+    temp.B_LINK = me.mention
+    DeendayalBot.username = '@' + me.username
+    DeendayalBot.loop.create_task(check_expired_premium(DeendayalBot))
     logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
     logging.info(LOG_STR)
+    logging.info(script.LOGO)
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    now = datetime.now(tz)
+    time = now.strftime("%H:%M:%S %p")
+    await DeendayalBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.B_LINK, today, time))
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app, bind_address, PORT).start()
     await idle()
-
+    
 if __name__ == '__main__':
     try:
-        loop.run_until_complete(Lazy_start())
-        logging.info('-----------------------🧐 Service running in Lazy Mode 😴-----------------------')
+        loop.run_until_complete(Deendayal_start())
     except KeyboardInterrupt:
-        logging.info('-----------------------😜 Service Stopped Sweetheart 😝-----------------------')
+        logging.info('Service Stopped Bye 👋')  
